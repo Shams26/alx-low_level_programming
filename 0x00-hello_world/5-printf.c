208fc44dcf0e136f3ef87c1c8058924d02d648cb
Shams26 <shamsudeenola6@gmail.com>
2023-02-16 09:42:33 -0800

    git

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - A program that prints a line, using the function printf
  * Return: 0 (success)
- * */
+ */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
