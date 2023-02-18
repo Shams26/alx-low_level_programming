f23cc37757f0fcc19fa4f255beeab520249cc6f3
Shams26 <shamsudeenola@gmail.com>
2023-02-18 02:20:44 -0800

    sa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,7 +4,7 @@
 /**
  * main - main block
  * Description: prints all single digit number of base 10
- * starting from 0. followed by a new line.
+ * starting from 0, followed by a new line.
  * Return: 0
  */
 int main(void)
